7bde7efe37a5577f17c460951930b224010f1a32
alexis-995 <alexis_d_07@hotmail.com>
2020-06-04 20:01:30 -0500

    Implementacion preliminar del servicio de Stream

---
+++ UVFYStream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UVFYStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
